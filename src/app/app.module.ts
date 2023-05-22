39075c723ee7683ad1f53d1ebc404b1df9c9e855
iamausafhussain <ausafhussain010@gmail.com>
2023-05-22 10:59:48 +0530

    att created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './widgets/navbar/navbar.component';
+import { FooterComponent } from './widgets/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HomeComponent, NavbarComponent, FooterComponent, AboutComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
