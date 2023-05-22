15817395410840ac89e067b8c416b36be74638a8
iamausafhussain <ausafhussain010@gmail.com>
2023-05-22 13:08:46 +0530

    added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,19 @@ import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './widgets/navbar/navbar.component';
 import { FooterComponent } from './widgets/footer/footer.component';
 import { AboutComponent } from './components/about/about.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NavbarComponent, FooterComponent, AboutComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    AboutComponent,
+  ],
+  imports: [BrowserModule, BrowserAnimationsModule, MatIconModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/widgets/navbar/navbar.component.ts
+++ src/app/widgets/navbar/navbar.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
-
+export class NavbarComponent implements OnInit {
+  constructor(private renderer: Renderer2) {}
+  ngOnInit(): void {
+    const script = this.renderer.createElement('script');
+    script.src = 'assets/navbar.js';
+    script.type = 'text/javascript';
+    script.charset = 'utf-8';
+    this.renderer.appendChild(document.head, script);
+  }
 }
